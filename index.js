a1974b25205b8cc120d5a82ac6cb607efdef668c
Kelsey Fecho <kfecho30@gmail.com>
2018-06-26 16:56:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-fetch('https://api.github.com/user/repos').
-  then(res => res.json()).
-  then(json => console.log(json))
+const token = ' 3c1cd86a8d162ead8772f0862a330e5f20881d40'
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
